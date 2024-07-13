74efe062e56c3a43292bd9b7933995eb2376df9c
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-07-13 20:25:30 +0530

    beautify the code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import Container from './components/Container';
-import Header from './components/Header';
+import React, { useState, useEffect } from "react";
+import Container from "./components/Container";
+import Header from "./components/Header";
 
 const App = () => {
   const [saplingIn_Out, setSaplingIn_Out] = useState([]);
   const [saplingMaster, setSaplingMaster] = useState([]);
 
   useEffect(() => {
- 
-    fetch('/saplinginwardoutward.json')
+    fetch("/saplinginwardoutward.json")
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Response is not coming');
+          throw new Error("Response is not coming");
         }
         return response.json();
       })
       .then((data) => {
         setSaplingIn_Out(data);
       })
-      .catch((error) => console.error('Error fetching saplinginwardoutward.json:', error));
+      .catch((error) =>
+        console.error("Error fetching saplinginwardoutward.json:", error)
+      );
 
-    
-    fetch('/saplings_master.json')
+    fetch("/saplings_master.json")
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Response is not coming');
+          throw new Error("Response is not coming");
         }
         return response.json();
       })
       .then((data) => {
         setSaplingMaster(data);
       })
-      .catch((error) => console.error('Error fetching saplings_master.json:', error));
+      .catch((error) =>
+        console.error("Error fetching saplings_master.json:", error)
+      );
   }, []);
 
   return (
-    <div className='w-[100%] my-[100px] mx-auto'>
-      <Header/>
-     <Container sapling_master_data={saplingMaster} saplingIn_Out_data={saplingIn_Out}/>
+    <div className="w-[100%] my-[100px] mx-auto">
+      <Header />
+      <Container
+        sapling_master_data={saplingMaster}
+        saplingIn_Out_data={saplingIn_Out}
+      />
     </div>
   );
 };
